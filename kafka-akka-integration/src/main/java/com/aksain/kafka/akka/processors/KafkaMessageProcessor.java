39b4e77d3846612504148213edd95f359818c2d6
Rustam Ismailov <rismailov@competo.io>
2018-12-19 14:13:30 +0400

    Added gradle build and settings script, fixed deprecated method

--- kafka-akka-integration/src/main/java/com/aksain/kafka/akka/processors/KafkaMessageProcessor.java
+++ kafka-akka-integration/src/main/java/com/aksain/kafka/akka/processors/KafkaMessageProcessor.java
@@ -5,6 +5,8 @@ import com.aksain.kafka.akka.main.KafkaMessageProducer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 
+import java.time.Duration;
+
 /**
  * @author Amit Kumar
  */
@@ -17,7 +19,7 @@ public class KafkaMessageProcessor extends KafkaMessageConsumer {
     public void run() {
         while(!isInterrupted()) {
             // Pull messages from request-topic and send to response-topic
-            final ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(1000);
+            final ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofMillis(1000) /*1000*/);
             for(ConsumerRecord<String, String> record : consumerRecords.records(responseTopic)) {
                 System.out.println("[Processor]Received message with key: " + record.key());
                 KafkaMessageProducer.getInstance().sendMessage(RESPONSE_TOPIC_NAME, record.key(), record.value());
